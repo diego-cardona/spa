024c788ddb1a0713fb667903748c1448119117db
CEIBA\diego.cardona <diacardonaco@gmail.com>
2021-01-04 08:32:02 -0500

    INADN-6677 Se eliminan imports sin usar de ComandoCita.

--- microservicio/aplicacion/src/main/java/com/ceiba/cita/comando/ComandoCita.java
+++ microservicio/aplicacion/src/main/java/com/ceiba/cita/comando/ComandoCita.java
@@ -1,23 +1,14 @@
 package com.ceiba.cita.comando;
 
-
-import com.ceiba.serviciospa.comando.ComandoServicioSpa;
 import com.ceiba.serviciospa.modelo.entidad.ServicioSpa;
-import com.ceiba.usuario.comando.ComandoUsuario;
 import com.ceiba.usuario.modelo.entidad.Usuario;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Getter
 @Setter
-//@AllArgsConstructor
-//@NoArgsConstructor
 public class ComandoCita implements Serializable {
     private Long id;
     private Usuario usuario;
